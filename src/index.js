968d4a7b8d65ac651d32906a41c85da1e593d997
YUNJINMOK <aprnaporn114@naver.com>
2024-04-16 10:18:19 +0900

    asd

--- src/index.js
+++ src/index.js
@@ -18,22 +18,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(
-    session({
-        name: 'Session ID',
-        secret: 'secret',
-        resave: false, // 세션이 변경되지 않아도 항상 저장하도록 설정
-        saveUninitialized: false, // 초기화되지 않은 세션을 저장소에 저장하지 않도록 설정
-        cookie: {
-            maxAge: 1000 * 60 * 60 * 24,
-            httpOnly: false, // javascript에서 사용이 안되게 하는 옵션
-            secure: false, // https를 통해서만 세션 쿠키를 전송하도록 설정
-        },
-        store: MongoStore.create({
-            mongoUrl: process.env.DB_URL + '/yj4project2',
-        }),
-    })
-);
+// app.use(
+//     session({
+//         name: 'Session ID',
+//         secret: 'secret',
+//         resave: false, // 세션이 변경되지 않아도 항상 저장하도록 설정
+//         saveUninitialized: false, // 초기화되지 않은 세션을 저장소에 저장하지 않도록 설정
+//         cookie: {
+//             maxAge: 1000 * 60 * 60 * 24,
+//             httpOnly: false, // javascript에서 사용이 안되게 하는 옵션
+//             secure: false, // https를 통해서만 세션 쿠키를 전송하도록 설정
+//         },
+//         store: MongoStore.create({
+//             mongoUrl: process.env.DB_URL + '/yj4project2',
+//         }),
+//     })
+// );
 
 app.get('/', function (req, res) {
     res.send('Hello World!!!!!');
